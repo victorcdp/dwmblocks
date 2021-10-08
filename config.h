03a42c5c94cbaa76ac02457dc23ac83bf3e5373f
victor cordeiro <victor.cdp97@gmail.com>
2021-10-08 14:29:33 -0300

    forked from void dwmblocks

--- config.h
+++ config.h
@@ -1,16 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+/* modify this file to change what commands output to your statusbar, and recompile using the make command. */
+
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-music",	0,	11},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-clock",	1,	1},
-	{"",	"sb-internet",	5,	4},
+    /* Icon         Command                 Interval    Signal */
+   /* { "",           "sb-ram",               60,         0       }, */
+    { "",           "sb-music",          1,         0       },
+    { "",           "sb-datetime",          1,         0       },
+    { "",           "sb-volume",            0,          10      },
+    /* { "",           "sb-battery",           60,         0       }, */
+    { "",           "sb-internet",          60,         0       },
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " | ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+/* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
+static char delim[] = "  ";
+static unsigned int delimLen = 4;
